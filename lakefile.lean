b4edfcd9d14853716d6b11319f9e473b001c894e
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-02-16 21:52:30 -0300

    build: fix Lake StdVer version typing

--- lakefile.lean
+++ lakefile.lean
@@ -2,9 +2,8 @@ import Lake
 open Lake DSL
 
 package overlapRigidityLean where
-  version := "0.1.0"
+  version := { major := 0, minor := 1, patch := 0 }
 
 lean_lib OverlapRigidityLean where
   roots := #[`OverlapRigidityLean]
-
 
